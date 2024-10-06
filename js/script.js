4170855c136b3f1e1105fdd4ba580fde59fa739d
karankv02 <karanvakkalad02@gmail.com>
2024-10-07 00:11:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let songs;
 let currFolder;
 async function getSongs(folder) {
   currFolder = folder;
-  let a = await fetch(`/${folder}/`);
+  let a = await fetch(`https://karankv02.github.io/spotify-clone/${folder}/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -79,7 +79,7 @@ const playMusic = (track, pause = false) => {
   
 };
 async function displayAlbums(){
-  let a = await fetch(`http://127.0.0.1:5500/songs/`);
+  let a = await fetch(`https://karankv02.github.io/spotify-clone/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -91,7 +91,7 @@ async function displayAlbums(){
       
     if(e.href.includes("/songs/")){
       let folder = e.href.split("/").slice(-1)[0]
-      let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
+      let a = await fetch(`https://karankv02.github.io/spotify-clone/songs/${folder}/info.json`);
       let response = await a.json();
       cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}"class="card">
               <div class="play">
