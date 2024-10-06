e0d65170c615e3c6a043ce94e6942aae2a6780b9
karankv02 <karanvakkalad02@gmail.com>
2024-10-07 00:53:02 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ const playMusic = (track, pause = false) => {
   
 };
 async function displayAlbums(){
-  let a = await fetch(`/songs/`);
+  let a = await fetch(`http://127.0.0.1:5500/songs/`);
   let response = await a.text();
   let div = document.createElement("div");
   div.innerHTML = response;
@@ -89,9 +89,9 @@ async function displayAlbums(){
     for (let index = 0; index < array.length; index++) {
       const e = array[index];
       
-    if(e.href.includes("/songs/") && !e.href.includes(".htaccess")){
+    if(e.href.includes("/songs/")){
       let folder = e.href.split("/").slice(-1)[0]
-      let a = await fetch(`/songs/${folder}/info.json`);
+      let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
       let response = await a.json();
       cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}"class="card">
               <div class="play">
